803036518fe6233875cb8e11d10cce0838fb4d0b
John Tunison <John.Tunison@e2open.com>
2015-01-12 12:21:16 -0600

    Add s3 diffing code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # stopgo
 Static site / pdf generator for your resume/cv, written in go
+
+stopgo = resume.  Get it?
+
+This code will take a resume definition in JSON format and generate a PDF and a static website.
+
+Features:
+* QR code in PDF points to website
+* Publish to S3 (with diffing md5 hashes, so it does quick updates)
---
+++ s3.go
@@ -0,0 +1,104 @@
+package stopgo
+
+import (
+	"crypto/md5"
+	"io/ioutil"
+	"launchpad.net/goamz/aws"
+	"launchpad.net/goamz/s3"
+	"log"
+	"mime"
+	"os"
+	"path/filepath"
+	"strings"
+	"encoding/hex"
+)
+
+func RetrieveS3Map(b *s3.Bucket) map[string]string {
+	var m = make(map[string]string)
+
+	res, err := b.List("", "", "", 1000)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, v := range res.Contents {
+		m[v.Key] = strings.Replace(v.ETag, `"`, "", -1)
+	}
+
+	return m
+}
+
+func Publish(localpath, bucketName, awsAccessKey, awsSecretKey string) error {
+
+	auth := aws.Auth{
+		AccessKey: awsAccessKey,
+		SecretKey: awsSecretKey,
+	}
+
+	connection := s3.New(auth, aws.USEast)
+	b := connection.Bucket(bucketName)
+	s3map := RetrieveS3Map(b)
+
+	// step 1:  copy all files that aren't already there
+	err := filepath.Walk("public", func(path string, fi os.FileInfo, _ error) error {
+
+		name := filepath.Base(path)
+		hidden := strings.HasPrefix(name, ".")
+		if !fi.IsDir() && !hidden {
+
+			// has it changed?
+
+			content, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			// have to omit the leading 'public/'
+			ext := filepath.Ext(name)
+			contentType := mime.TypeByExtension(ext)
+			key := path[7:]
+			md5bytes := md5.Sum(content)
+			md5string := hex.EncodeToString(md5bytes[:])
+
+			if s3map[key] == "" {
+				log.Printf("Adding '%s'\n", key)
+				err = b.Put(key, content, contentType, s3.PublicRead)
+				if err != nil {
+					return err
+				}
+
+			} else if s3map[key] != md5string {
+				log.Printf("Updating '%s'\n", key)
+				// b.Del(key)
+				// if err != nil {
+				// 	return err
+				// }
+				err = b.Put(key, content, contentType, s3.PublicRead)
+				if err != nil {
+					return err
+				}
+
+			} 
+			// else {
+			// 	log.Printf("Skipping '%s' (unchanged)\n", key)
+			// }
+
+			delete(s3map, key)
+
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
+	// step 2: remove any s3 files that are no longer relevant
+	for key, _ := range s3map {
+		log.Printf("Deleting '%s'\n", key)
+		err = b.Del(key)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+
+}
